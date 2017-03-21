0641ffda3a642fbb38688be3a5f04c4e29f2f272
Callum <callummance@gmail.com>
2017-03-21 09:31:43 +0000

    Add libsharlayan dependancy in stack config

--- src/Data/Krile.hs
+++ src/Data/Krile.hs
@@ -2,6 +2,8 @@
 module Data.Krile (krile)
   where
 
+import Data.Krile.FFData
+
 -- |The entry point for the program
 krile :: IO ()
 krile = print "uwot?"
---
+++ src/Data/Krile/FFData.hs
@@ -0,0 +1,6 @@
+-- |Processing of data from the lodestone and database
+module Data.Krile.FFData
+  where
+
+
+
